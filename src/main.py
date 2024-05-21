71c5d9b1c596adcdf407d998dc0bcef7b23c04fe
dunossauro <mendesxeduardo@gmail.com>
2024-05-21 17:43:38 -0300

    Distribuindo em routers

--- src/main.py
+++ src/main.py
@@ -1,93 +1,9 @@
-from nicegui import ui
+from nicegui import ui, app
 import datetime
 from utils import db
 import utils.models as models
 
 from sqlalchemy import exc, select
-
-
-@ui.page('/cliente_cadastro')
-def new_customer_page():
-    ui.markdown('# Cadastro Cliente')
-
-    name = ui.input(label='Nome',
-                    placeholder='entre com o nome...',
-                    on_change=lambda e: e.value)
-    
-    lastname = ui.input(label='Sobrenome',
-                    placeholder='entre com o sobrenome...',
-                    on_change=lambda e: e.value)
-
-    with ui.input('Aniversário') as birthday:
-        with birthday.add_slot('append'):
-            ui.icon('edit_calendar').on('click', lambda: menu.open()).classes('cursor-pointer')
-        with ui.menu() as menu:
-            ui.date().bind_value(birthday)
-
-    email = ui.input(label='Email',
-                    placeholder='entre com o email...',
-                    on_change=lambda e: e.value)
-
-    document_id = (ui.input(label="CPF",
-                           placeholder="entre com o cpf",
-                           validation={'Entre com 11 valores para cpf': lambda value: len(value) == 14}
-                           )
-                      .props('mask="###.###.###-##"'))
-
-    phone1 = (ui.input(label='Telefone Cel. (WhatsApp)',
-                       placeholder='entre com o número de telefone...',
-                       on_change=lambda e: e.value,
-                       validation={'Entre com 14 valores para telefone': lambda value: len(value) == 18}
-                       )
-                .props('mask="+55(##)#.####.####"'))
-    
-    phone2 = (ui.input(label='Telefone Comercial',
-                    placeholder='entre com o número de telefone...',
-                    validation={'Entre com 14 valores para telefone': lambda value: len(value) == 18},
-                    on_change=lambda e: e.value)
-                .props('mask="+55(##)#.####.####"'))
-    
-    instagram = ui.input(label='Instagram',
-                         placeholder='entre com o @ do instagram...',
-                         on_change=lambda e: e.value)
-
-    def make_signup():
-        mandatory_values = [
-            name.value,
-            lastname.value,
-            email.value,
-            birthday.value,
-            document_id.value
-        ]
-
-        if '' in mandatory_values:
-            ui.notify("Entre com os dados necessários", color='red')
-            return
-
-        customer = models.Customer(name=name.value,
-                                    lastname=lastname.value,
-                                    email=email.value,
-                                    date=datetime.datetime.strptime(birthday.value, "%Y-%m-%d"),
-                                    document=document_id.value,
-                                    phone1=phone1.value,
-                                    phone2=phone2.value,
-                                    instagram=instagram.value)
-
-        try:
-            session.add(customer)
-            session.commit()
-            ui.notify("Cadastro realizado com sucesso!!", color='green')
-
-
-        except exc.IntegrityError as err:
-            print(err)
-            session.rollback()
-            ui.notify("Usuário já existente", color='red')
-
-    with ui.row():
-        ui.button("Cadastrar!", on_click=make_signup)
-        ui.button("Início", on_click=lambda: ui.navigate.to('/'))
-
 
 @ui.page('/cliente_busca')
 def search_customer_page(error=None):
@@ -254,5 +170,10 @@ ui.markdown('## Produtos')
 with ui.row():
     # ui.button('Cadastrar', on_click=get_go)
     ui.button('Buscar', on_click=lambda: ui.notify('Buscando produto'))
+
+
+# AQUI!!!
+from routers.cadastro import cadastro
+app.include_router(cadastro)
 
 ui.run(port=8081)
